9910aa9deb770d5dead4736bca894bb055b9d165
mohitkadwe19 <118180431+mohitkadwe@users.noreply.github.com>
2023-11-16 01:06:00 +0530

    change the image remote pattern in next config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,12 @@ const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
     images: {
-        domains: ['images.unsplash.com'],
+        remotePatterns : [
+            {
+                protocol : 'https',
+                hostname : 'images.unsplash.com'
+            }
+        ]
     },
 }
 
